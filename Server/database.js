206b6605cc57848d00ac325b5a2dda912904cb56
Amna Abdallah <amnaabdallah@Amnas-Air.lan>
2024-07-05 20:53:31 -0400

    database created and react initiated for client end

--- Server/database.js
+++ Server/database.js
@@ -2,12 +2,103 @@ const pg = require('pg');
 const client = new pg.Client('postgres://localhost/daily_goal_db');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const JWT = 'shhh';
+
+const createTables = async()=> {
+    const SQL = `
+      DROP TABLE IF EXISTS goals;  
+      DROP TABLE IF EXISTS users;
+      CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(20) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE goals(
+        id UUID PRIMARY KEY,
+        goal VARCHAR(255),
+        user_id UUID REFERENCES users(id) NOT NULL,
+        CONSTRAINT unique_user UNIQUE (user_id)
+      );
+    `;
+    await client.query(SQL);
+  };
+
+  const createUser = async({ username, email, password})=> {
+    const SQL = `
+      INSERT INTO users(id, username, email, password) VALUES($1, $2, $3, $4) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), username, email, await bcrypt.hash(password, 5)]);
+    return response.rows[0];
+  };
+
+  const createGoal = async({ user_id, goal })=> {
+    const SQL = `
+      INSERT INTO goals(id, user_id, goal) VALUES($1, $2, $3) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), user_id, goal]);
+    return response.rows[0];
+  };
+
+  const knockoutGoal = async({ user_id, id })=> {
+    const SQL = `
+      DELETE FROM goals WHERE user_id=$1 AND id=$2
+    `;
+    await client.query(SQL, [user_id, id]);
+  };
+
+  const fetchUser = async(email) => {
+    const SQL = `
+      SELECT * FROM users
+      WHERE email = $1
+    `;
+    const response = await client.query(SQL, [email]);
+    return response.rows[0];
+  }
+
+  const fetchGoals = async(user_id)=> {
+    const SQL = `
+      SELECT * FROM goals where user_id = $1
+    `;
+    const response = await client.query(SQL, [user_id]);
+    return response.rows;
+  };
+
+  const updateGoal = async({goal_id, goal})=> {
+    const SQL = `
+    UPDATE goals
+    SET goal = $1
+    WHERE id = $2 
+    RETURNING *;
+    `;
+    const response = await client.query(SQL, [ goal, goal_id ]);
+    return response.rows[0];
+  }
+
+  const deleteUser = async({ user_id })=> {
+    const SQL = `
+      DELETE FROM users WHERE id=$1;
+    `;
+    await client.query(SQL, [user_id]);
+  };
+
+  const deleteGoals = async({ user_id })=> {
+    const SQL = `
+      DELETE FROM goals WHERE id=$1;
+    `;
+    await client.query(SQL, [user_id]);
+  };
 
 
 
 module.exports = {
     client,
-    createTables
+    createTables,
+    createUser,
+    createGoal,
+    knockoutGoal,
+    fetchUser,
+    fetchGoals,
+    updateGoal,
+    deleteUser,
+    deleteGoals
 };
--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,108 @@
-const {client, createTables} = require('./database');
+const {client, createTables, createUser, createGoal, knockoutGoal, fetchUser, fetchGoals, updateGoal, deleteUser, deleteGoals} = require('./database');
 const express = require('express');
 const app = express();
 app.use(express.json());
+const jwt = require("jsonwebtoken");
+const SECRET = "fmyumchsymxgnbfgtmugfnym";
+
+
+
+
+app.post('/api/register', async(req,res,next)=> {
+    try {
+        const checkCustomer = await fetchUser(req.body.email);
+        if(!checkCustomer) {
+            const newCostomer = await createUser(req.body);
+        const token = jwt.sign(newCostomer.id, SECRET);
+        res.send({token, newCostomer});
+        } else {
+            res.send("try logging in!")
+        }
+    } catch(ex){
+        next(ex); 
+    } 
+});
+
+app.post('/api/login', async(req,res,next)=> {
+    try {
+        const checkCustomer = await fetchUser(req.body.email);
+        if(!checkCustomer) {
+            res.send("Sorry, you are not a user, try signing up");
+        } else if (!(await bcrypt.compare(req.body.password, checkCustomer.password))) {
+            res.send("try logging in!")
+        } else {
+            const token = jwt.sign(checkCustomer.id, SECRET);
+            res.send({token, checkCustomer});
+        }
+    } catch(ex){
+        next(ex); 
+    } 
+});
+
+app.post('/api/user/:id/goal', async(req, res, next)=> {
+    try {
+      res.status(201).send(await createGoal({ user_id: req.params.id, goal: req.body.goal}));
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+app.get('/api/:id/mygoals', async(req, res, next)=> {
+    try {
+      res.send(await fetchGoals(req.params.id));
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+app.patch('/api/goal/:id', async(req, res, next)=>{
+    try {
+        res.status(201).send(await updateGoal({goal_id: req.params.id, goal: req.body.goal}));
+    } catch(ex){
+        next(ex);
+    }
+});  
+
+app.delete('/api/user/:userId/goal/:id', async(req, res, next)=> {
+    try {
+      await knockoutGoal({ id: req.params.id, user_id: req.params.userId });
+      res.sendStatus(204);
+    }
+    catch(ex){
+      next(ex);
+    }
+  });
+
+  app.delete('/api/user/:userId', async(req, res, next)=> {
+        try {
+            await deleteUser({ user_id: req.params.userId });
+            res.sendStatus(204);
+          }
+          catch(ex){
+            next(ex);
+          }
+  });
+
+  app.delete('/api/user/:userID', async(req, res, next)=> {
+    try {
+        await deleteGoals({ user_id: req.params.userID });
+        res.sendStatus(204);
+      }
+      catch(ex){
+        next(ex);
+      }
+});
+
+    const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+  
+    const port = 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  
+  };
+  
+  init();
